f06620a2de0b7c99e5e9a9eeb969423984aa1ea7
furkanyesilyurt <52417156+furkanyesilyurt@users.noreply.github.com>
2021-10-18 16:37:20 +0300

    mayin tarlasi

---
+++ mayinTarlasi/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input  = new Scanner(System.in);
+
+        System.out.print("Satır sayısını giriniz: ");
+        int row = input.nextInt();
+        System.out.print("Sütun sayısını giriniz: ");
+        int column = input.nextInt();
+
+
+        MineSweeper mine = new MineSweeper(row,column);
+        mine.run();
+
+    }
+}
